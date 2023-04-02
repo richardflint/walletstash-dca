f01f149f002a116eef259685fe7afa1e4d5da54c
Richard Flint <richard@richardflint.co.uk>
2023-04-02 17:07:32 +0100

    reduce sleep

--- server/test/exchange-configurations.e2e-spec.ts
+++ server/test/exchange-configurations.e2e-spec.ts
@@ -64,7 +64,7 @@ describe('ExchangeConfigurationsController (e2e)', () => {
     app.setGlobalPrefix('api');
     await app.init();
     createTestDataSet();
-    await delay(250);
+    await delay(125);
   });
 
   it('create [POST /exchange-configurations]', async () => {
