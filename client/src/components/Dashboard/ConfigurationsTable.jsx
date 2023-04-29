c67e7495dfb1a6956e189a707995735a64091cdf
Richard Flint <richard@richardflint.co.uk>
2023-04-29 09:24:24 +0100

    make delete button red

--- client/src/components/Dashboard/ConfigurationsTable.jsx
+++ client/src/components/Dashboard/ConfigurationsTable.jsx
@@ -60,7 +60,7 @@ const ConfigurationsTable = ({ configurations = [], onDelete, onEdit }) => {
             <td>
               <button
                 data-testid="delete-button"
-                className="inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                className="inline-flex justify-center rounded-md border border-transparent bg-red-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2"
                 onClick={() => onDelete(configuration.id)}
               >
                 Delete
